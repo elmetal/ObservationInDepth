b2078d3a7de89ed2f33e49d0e903dfbd8201cf48
elmetal <nm7.pupu@gmail.com>
2023-10-25 18:11:00 +0900

    the first step of inline macro

---
+++ Sources/ObservationInDepth/Documentation.docc/Tutorials/NestedObservableObject-06.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+import Combine
+
+final class Parent {
+    @ObservationTracked var child: Child
+
+    init(child: Child) {
+        self.child = child
+    }
+
+
+    @ObservationIgnored private let _$observationRegistrar = Observation.ObservationRegistrar()
+
+    internal nonisolated func access<Member>(
+        keyPath: KeyPath<Parent , Member>
+    ) {
+        _$observationRegistrar.access(self, keyPath: keyPath)
+    }
+
+    internal nonisolated func withMutation<Member, MutationResult>(
+        keyPath: KeyPath<Parent , Member>,
+        _ mutation: () throws -> MutationResult
+    ) rethrows -> MutationResult {
+        try _$observationRegistrar.withMutation(of: self, keyPath: keyPath, mutation)
+    }
+}
+
+extension Parent: Observation.Observable {
+}
+
+
+final class Child {
+    @ObservationTracked var age: Int
+
+    init(age: Int) {
+        self.age = age
+    }
+
+    func haveBirthday() {
+        age += 1
+    }
+
+
+    @ObservationIgnored private let _$observationRegistrar = Observation.ObservationRegistrar()
+
+    internal nonisolated func access<Member>(
+        keyPath: KeyPath<Child , Member>
+    ) {
+        _$observationRegistrar.access(self, keyPath: keyPath)
+    }
+
+    internal nonisolated func withMutation<Member, MutationResult>(
+        keyPath: KeyPath<Child , Member>,
+        _ mutation: () throws -> MutationResult
+    ) rethrows -> MutationResult {
+        try _$observationRegistrar.withMutation(of: self, keyPath: keyPath, mutation)
+    }
+}
+
+extension Child: Observation.Observable {
+}
